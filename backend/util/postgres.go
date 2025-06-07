1ef9f649c7c0563066139afcd0b49675cf71b62b
Fernando Entrena Rivas <81759354+fentrena@users.noreply.github.com>
2025-06-07 23:48:07 +0100

    Update postgres.go

--- backend/util/postgres.go
+++ backend/util/postgres.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,23 +11,19 @@ import (
 var DB *gorm.DB
 var err error
 
-// InitDB initializes the database connection
+// InitDB initializes the database connection using DATABASE_URL
 func InitDB() {
-	dbHost := os.Getenv("DB_HOST")
-	dbUser := os.Getenv("POSTGRES_USER")
-	dbPassword := os.Getenv("POSTGRES_PASSWORD")
-	dbName := os.Getenv("POSTGRES_DATABASE")
-	dbPort := os.Getenv("POSTGRES_PORT")
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
-		dbHost, dbUser, dbPassword, dbName, dbPort)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
 
-	log.Println("Successfully connected to the database.")
+	log.Println("✅ Successfully connected to the database.")
 }
 
 // GetDB returns the database connection
