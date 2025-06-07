994106ee099fa5c6862dc2b90f3dd8b09281b97d
Fernando Entrena Rivas <81759354+fentrena@users.noreply.github.com>
2025-06-08 00:13:22 +0100

    Update postgres.go

--- backend/util/postgres.go
+++ backend/util/postgres.go
@@ -9,24 +9,22 @@ import (
 )
 
 var DB *gorm.DB
-var err error
 
-// InitDB initializes the database connection using DATABASE_URL
 func InitDB() {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		log.Fatal("DATABASE_URL not set")
 	}
 
+	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
 
-	log.Println("✅ Successfully connected to the database.")
+	log.Println("Successfully connected to the database.")
 }
 
-// GetDB returns the database connection
 func GetDB() *gorm.DB {
 	return DB
 }
